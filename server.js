baa10678e7636b12b8339f6d8b7002883e167dda
Brochb <brochbaltzer@gmail.com>
2023-09-14 10:55:16 -0600

    adding functionality to server, close to getting function"

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+
+app.get('/send', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/routes', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/api/notes', (req, res) => res.json()
+    .res.sendFile(path.join(__dirname, 'public/assets/index.html')));
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
