b82c33f869a9370eec006e971533c491af926ce9
Brochb <brochbaltzer@gmail.com>
2023-09-16 18:38:01 -0600

    added routes, getting closer to full functionality

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const path = require('path');
+const notesData = require('./db/db.json')
+const fs = require('fs')
+// const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
-
-app.get('/send', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/routes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) => res.json()
-    .res.sendFile(path.join(__dirname, 'public/assets/index.html')));
+app.get('/api/notes', (req, res) => { res.json(notesData);
+  res.readFile(path.join(__dirname, 'public/db/db.json'))
+
+});
+
+app.post('/api/notes', (req, res) => { res.json(notesData)
+  res.writeFile(path.join(__dirname, 'public/db/db.json'))
+    console.log('Post Success')
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
